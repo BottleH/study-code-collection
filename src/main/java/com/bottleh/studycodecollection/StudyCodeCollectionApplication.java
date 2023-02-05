2142f68fd2391c46f95d4291deb8fcd6b3ea6546
kimbyeonghwan <guys0217@gmail.com>
2023-02-05 16:13:11 +0900

    repo 최초 구성
    - gradle 추가

---
+++ src/main/java/com/bottleh/studycodecollection/StudyCodeCollectionApplication.java
@@ -0,0 +1,13 @@
+package com.bottleh.studycodecollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyCodeCollectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyCodeCollectionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bottleh/studycodecollection/StudyCodeCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bottleh.studycodecollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyCodeCollectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
