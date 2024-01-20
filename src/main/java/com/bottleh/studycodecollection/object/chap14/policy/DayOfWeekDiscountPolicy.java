34e4c3559974a698cd61e31af851748f20d41478
kimbyeonghwan <guys0217@gmail.com>
2024-01-20 17:38:45 +0900

    [Object] chapter14 코드 수정

--- src/main/java/com/bottleh/studycodecollection/object/chap14/policy/DayOfWeekDiscountPolicy.java
+++ src/main/java/com/bottleh/studycodecollection/object/chap14/policy/DayOfWeekDiscountPolicy.java
@@ -2,16 +2,19 @@ package com.bottleh.studycodecollection.object.chap14.policy;
 
 import com.bottleh.studycodecollection.object.chap14.domain.Call;
 import com.bottleh.studycodecollection.object.chap14.domain.DateTimeInterval;
+import com.bottleh.studycodecollection.object.chap14.fee.FeeRule;
 import com.bottleh.studycodecollection.object.chap2.Money;
-import lombok.RequiredArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@RequiredArgsConstructor
 public class DayOfWeekDiscountPolicy extends BasicRatePolicy {
 
     private final List<DayOfWeekDiscountRule> rules = new ArrayList<>();
+
+    protected DayOfWeekDiscountPolicy(List<FeeRule> feeRules) {
+        super(feeRules);
+    }
 
     @Override
     protected Money calculateCallFee(Call call) {
