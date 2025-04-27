c5a46282864f55c30abc32f9129163d311a15847
kimbyeonghwan <guys0217@gmail.com>
2025-04-27 12:03:58 +0900

    [kotlin] chapter9 코드 추가

---
+++ src/main/java/com/bottleh/studycodecollection/kotlin/chap9/chap9.kt
@@ -0,0 +1,118 @@
+package com.bottleh.studycodecollection.kotlin.chap9
+
+import java.lang.reflect.Type
+import java.math.BigDecimal
+import java.time.LocalDate
+import kotlin.reflect.KProperty
+
+data class Point(val x: Int, val y: Int) {
+    operator fun plus(other: Point): Point {
+        return Point(x + other.x, y + other.y)
+    }
+}
+
+operator fun Point.times(scale: Double): Point {
+    return Point((x * scale).toInt(), (y * scale).toInt())
+}
+
+operator fun Char.times(count: Int): String {
+    return toString().repeat(count)
+}
+
+operator fun <T> MutableCollection<T>.plusAssign(element: T) {
+    this.add(element)
+}
+
+operator fun Point.unaryMinus(): Point {
+    return Point(-x, -y)
+}
+
+operator fun BigDecimal.inc() = this + BigDecimal.ONE
+
+operator fun Point.get(index: Int): Int {
+    return when (index) {
+        0 -> x
+        1 -> y
+        else ->
+            throw IndexOutOfBoundsException("Invalid coordinate $index")
+    }
+}
+
+data class MutablePoint(var x: Int, var y: Int)
+
+operator fun MutablePoint.set(index: Int, value: Int) {
+    when (index) {
+        0 -> x = value
+        1 -> y = value
+        else ->
+            throw IndexOutOfBoundsException("Invalid coordinate $index")
+    }
+}
+
+data class Rectangle(val upperLeft: Point, val lowerRight: Point)
+
+operator fun Rectangle.contains(p: Point): Boolean {
+    return p.x in upperLeft.x..<lowerRight.x &&
+            p.y in upperLeft.y..<lowerRight.y
+}
+
+operator fun ClosedRange<LocalDate>.iterator(): Iterator<LocalDate> =
+    object : Iterator<LocalDate> {
+        var current = start
+        override fun hasNext(): Boolean =
+            current <= endInclusive
+
+        override fun next(): LocalDate {
+            val thisDate = current
+            current = current.plusDays(1)
+            return thisDate
+        }
+    }
+
+data class NameComponents(val name: String, val extension: String)
+
+fun splitFileName(fullName: String): NameComponents {
+    val (name, extension) = fullName.split('.', limit = 2)
+    return NameComponents(name, extension)
+}
+
+fun printEntries(map: Map<String, String>) {
+    for ((key, value) in map) {
+        println("$key -> $value")
+    }
+}
+
+fun interface Observer {
+    fun onChange(name: String, oldValue: Any?, newValue: Any?)
+}
+
+open class Observable {
+    val observers = mutableListOf<Observer>()
+    fun notifyObservers(propName: String, oldValue: Any?, newValue: Any?) {
+        for (obs in observers) {
+            obs.onChange(propName, oldValue, newValue)
+        }
+    }
+}
+
+class ObservableProperty(var propValue: Int, val observable: Observable) {
+    operator fun getValue(thisRef: Any?, prop: KProperty<*>): Int = propValue
+    operator fun setValue(thisRef: Any?, prop: KProperty<*>, newValue: Int) {
+        val oldValue = propValue
+        propValue = newValue
+        observable.notifyObservers(prop.name, oldValue, newValue)
+    }
+}
+
+class Person {
+    private val _attributes = mutableMapOf<String, String>()
+
+    fun setAttribute(attrName: String, value: String) {
+        _attributes[attrName] = value
+    }
+
+    var name: String by _attributes
+}
+
+fun main() {
+}
